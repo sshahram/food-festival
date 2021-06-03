aa64831c51482c2e385da8c59cd8d4425d537887
sshahram <shirin.shahram23@gmail.com>
2021-06-02 23:12:57 -0700

    intercept fetch requests

--- service-worker.js
+++ service-worker.js
@@ -43,3 +43,21 @@ self.addEventListener('activate', function (e) {
         })
     );
 });
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) { // if caches is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+
+            // You can omit if/else for console.log & put one line below like this too
+            // return request || fetch(e.request)
+        })
+    )
+})
